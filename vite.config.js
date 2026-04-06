599966275d4e43f4d62ea59cf094c4f3f0773681
Ryanpaul-ai <magallanes.ryanpaulc11p@gmail.com>
2026-04-07 03:20:37 +0800

    Initial Commit

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   assetsInclude: ['**/*.PNG'],
   plugins: [react()],
+  optimizeDeps: {
+  exclude: ['@react-three/fiber']
+},
+   build: {
+    chunkSizeWarningLimit: 1600
+   }
+
 })
