bd656377a15038bd4f71888d61ddc45e4c029023
Ryanpaul-ai <magallanes.ryanpaulc11p@gmail.com>
2026-04-07 08:44:52 +0800

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   assetsInclude: ['**/*.PNG'],
-  plugins: [react()],
+  plugins: [tailwindcss(), react()],
   optimizeDeps: {
   exclude: ['@react-three/fiber']
 },
